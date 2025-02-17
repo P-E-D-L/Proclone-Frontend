b257d33f9d9596d5da9fbcd2e0383da04738af34
luke-danger <kimesluke@gmail.com>
2025-02-16 16:10:30 -0800

    base framework and hellow world

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
+        <p>
+          Welcome to PEDL's CS4800 project.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
